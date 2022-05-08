b27728252394686e71b7a464e0b925ff06de1213
webmaster-k0ara <webmaster.k0ala.jurimarikikiburu@gmail.com>
2022-05-08 17:21:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require("path");
+const app = express();
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,"public")));
+
+// app.get('/', function (req, res) {
+//   console.log(req);
+//   res.send('<h1>トップページ</h1>')
+// });
+
+app.post("/api/v1/quiz", function(req,res){
+  const answer = req.body.answer;
+  if(answer === "2"){
+    // res.send("<h1>正解</h1>")
+    res.redirect("/correct.html")
+  }else{
+    // res.send("<h1>不正解</h1>")
+    res.redirect("/wrong.html")
+  }
+});
+
+
+app.listen(3000,function(){
+  console.log('im ranning!');
+});
